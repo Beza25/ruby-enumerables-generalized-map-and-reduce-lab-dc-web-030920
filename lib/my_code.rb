f914d3a301b1727197138301c85b1cdb8bf3c8f1
Beza Sirak <sirak.bezawit@gmail.com>
2020-03-01 20:01:03 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,21 @@
 # Your Code Here
+
+def map(source_array) 
+  index = 0 
+  while index < source_array.length do 
+    yield(source_array[index])
+    index += 1 
+  end
+  source_array
+end
+
+map(source_array) { |element| element * -1 }
+map(source_array)
+map(source_array) { |element| element * 2 }
+map(source_array) { |element| element * element }
+  
+
+
+def reduce(source_array)
+  
+end
