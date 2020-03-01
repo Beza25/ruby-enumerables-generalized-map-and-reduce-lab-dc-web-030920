8bc44f2a85b4533a9025d2918e67afe019cbd24b
Beza Sirak <sirak.bezawit@gmail.com>
2020-03-01 20:34:48 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -13,13 +13,14 @@ def map(source_array)
   new_arr
 end
 
-map(source_array) { |element| element * -1 }
 map(source_array)
-map(source_array) { |element| element * 2 }
-map(source_array) { |element| element * element }
+# map(source_array) { |element| element * -1 }
+
+# map(source_array) { |element| element * 2 }
+# map(source_array) { |element| element * element }
   
 
 
-def reduce(source_array)
+# def reduce(source_array)
   
-end
+# end
